84d63a2020058316d7cb2c5f94d31cbd4dce19b1
Nick Stielau <nick.stielau@gmail.com>
2016-09-13 12:51:24 -0700

    Using resolve host in example.

--- example_remote_http.php
+++ example_remote_http.php
@@ -4,13 +4,30 @@
 // when building a full Drupal site, just so we don't worry about testing.
 setcookie('NO_CACHE', '1');
 
-print "Through the stunnel, hit the remote service";
+/////////////////
+$url = "https://site.com:443/v1/o/test";
+$soip_constant_name = "PANTHEON_SOIP_XXXXX_YOUR_SERVICE_NAME"
+////////////////
+
+print "Through the stunnel, hit the remote service\n";
+print "\n port : " . constant($soip_constant_name);
+
+// Create a "resolve_host" that will point to localhost and resolve externally
+$host = parse_url($url, PHP_URL_HOST);
+$localhost = "127.0.0.1";
+$resolve_host = array(sprintf("%s:%d:%s", $host, constant($soip_constant_name, $localhost));
+print "\n resolve_host : " . implode($resolve_host);
+
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "127.0.0.1");
-curl_setopt($ch, CURLOPT_PORT, PANTHEON_SOIP_XXXXX_YOUR_SERVICE_NAME);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RESOLVE, $resolve_host);
+curl_setopt($ch, CURLOPT_PORT, PHP_CONSTANT);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+curl_setopt($ch, CURLOPT_VERBOSE, TRUE);
 $start = microtime(true);
 $result = curl_exec($ch);
+$error = curl_error($ch);
 curl_close($ch);
 print "<h2>Contacting Remote Service</h2>";
 print "Queried remote service in " . (microtime(true) - $start) . " seconds";
